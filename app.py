c80fa986db57ed1fd40401d72073391a125a45ed
Piyush <piyushbhomale1999@gmail.com>
2022-03-03 21:02:30 +0530

    All Stacks Checked

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
-
+from Stacks.DataBase import Database
+from Stacks.Data_genrator import EC2
 from database.database_stack import DatabaseStack
+from Stacks.DataLoader import LoaderS3
 
+dev_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
 app = cdk.App()
-DatabaseStack(app, "DatabaseStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
+LoaderS3(app,"filesloader", env=dev_env)
+database = Database(app, "databasestack", env=dev_env)
+genrator = EC2(app, "datagenrator", env=dev_env)
+genrator.add_dependency(database, "database is required to put records")
 app.synth()

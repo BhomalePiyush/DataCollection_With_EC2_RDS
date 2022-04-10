1f805a16d967c9f1558332346ff62185a7396bf9
Piyush <piyushbhomale1999@gmail.com>
2022-04-10 10:42:26 +0530

    envornment configured

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from Stacks.DataLoader import LoaderS3
 from Stacks.ECS_Cluster import ECS
 
 dev_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
-prod_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+prod_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region='eu-west-1')
 
 app = cdk.App()
 database = Database(app, "databasestack", env=dev_env)
@@ -19,11 +19,11 @@ ecs_cluster.add_dependency(database, 'for creating ecs cluster database is requi
 # genrator = EC2(app, "datagenrator", env=dev_env)
 # genrator.add_dependency(loader, "database is required to put records")
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<production envornment>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# prod_database = Database(app, "Proddatabasestack", is_prod=True, env=dev_env)
-# prod_ecs_cluster = ECS(app, 'ecsstack', is_prod=True, env=dev_env)
+# prod_database = Database(app, "Proddatabasestack", is_prod=True, env=dev_env) # use command cdk deploy Proddatabasestack --profile prod
+# prod_ecs_cluster = ECS(app, 'ecsstack', is_prod=True, env=dev_env) # use command cdk deploy ecsstack --profile prod
 # prod_ecs_cluster.add_dependency(database, 'for creating ecs cluster database is required')
-# prod_loader = LoaderS3(app, "Prodfilesloader", is_prod=True, env=prod_env)
+# prod_loader = LoaderS3(app, "Prodfilesloader", is_prod=True, env=prod_env) # use command cdk deploy Prodfilesloader --profile prod
 # prod_loader.add_dependency(database, "database should be available to deploy s3")
-# prod_genrator = EC2(app, "Proddatagenrator", is_prod=True, env=dev_env)
+# prod_genrator = EC2(app, "Proddatagenrator", is_prod=True, env=dev_env) # use command cdk deploy Proddatagenrator --profile prod
 # prod_genrator.add_dependency(prod_loader, "database is required to put records")
 app.synth()

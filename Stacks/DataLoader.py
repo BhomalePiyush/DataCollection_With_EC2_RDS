e6e9a3c214f37dc20bc9e4f707d413c7aff7ea08
Piyush <piyushbhomale1999@gmail.com>
2022-04-10 09:28:50 +0530

    prod env and dev env added to project

--- Stacks/DataLoader.py
+++ Stacks/DataLoader.py
@@ -2,7 +2,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_s3_deployment as _s3Deploy,
-    aws_s3 as _s3
+    aws_s3 as _s3,
+    RemovalPolicy
     # aws_sqs as sqs,
 )
 
@@ -11,21 +12,38 @@ from constructs import Construct
 
 class LoaderS3(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, is_prod=False, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        if is_prod:
+            _bucket = _s3.Bucket(self,
+                                 'project-S3Bucket',
+                                 access_control=_s3.BucketAccessControl.PUBLIC_READ_WRITE,
+                                 bucket_name="piyushbhomalefirstclibucket",
+                                 public_read_access=True,
+                                 removal_policy=RemovalPolicy.RETAIN,
+                                 versioned=True)
 
-        _bucket = _s3.Bucket(self,
-                             'project-S3Bucket',
-                             access_control=_s3.BucketAccessControl.PUBLIC_READ_WRITE,
-                             bucket_name="piyushbhomalefirstclibucket",
-                             public_read_access=True)
+            _s3Deploy.BucketDeployment(self,
+                                       'bucket-datadeployment',
+                                       sources=[_s3Deploy.Source.asset(path="Scraper")
+                                                ],
+                                       destination_bucket=_bucket,
+                                       )
+        else:
+            _bucket = _s3.Bucket(self,
+                                 'project-S3Bucket',
+                                 access_control=_s3.BucketAccessControl.PUBLIC_READ_WRITE,
+                                 bucket_name="piyushbhomalefirstclibucket",
+                                 public_read_access=True,
+                                 removal_policy=RemovalPolicy.DESTROY)
 
-        _s3Deploy.BucketDeployment(self,
-                                   'bucket-datadeployment',
-                                   sources=[_s3Deploy.Source.asset(path="Scraper")
-                                            ],
-                                   destination_bucket=_bucket,
-                                   )
+            _s3Deploy.BucketDeployment(self,
+                                       'bucket-datadeployment',
+                                       sources=[_s3Deploy.Source.asset(path="Scraper")
+                                                ],
+                                       destination_bucket=_bucket,
+                                       )
+
 
 
 
--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from Stacks.DataLoader import LoaderS3
 from Stacks.ECS_Cluster import ECS
 
 dev_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+prod_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
 app = cdk.App()
 database = Database(app, "databasestack", env=dev_env)
@@ -17,4 +18,12 @@ ecs_cluster.add_dependency(database, 'for creating ecs cluster database is requi
 # loader.add_dependency(database, "database should be available to deploy s3")
 # genrator = EC2(app, "datagenrator", env=dev_env)
 # genrator.add_dependency(loader, "database is required to put records")
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<production envornment>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# prod_database = Database(app, "Proddatabasestack", is_prod=True, env=dev_env)
+# prod_ecs_cluster = ECS(app, 'ecsstack', is_prod=True, env=dev_env)
+# prod_ecs_cluster.add_dependency(database, 'for creating ecs cluster database is required')
+# prod_loader = LoaderS3(app, "Prodfilesloader", is_prod=True, env=prod_env)
+# prod_loader.add_dependency(database, "database should be available to deploy s3")
+# prod_genrator = EC2(app, "Proddatagenrator", is_prod=True, env=dev_env)
+# prod_genrator.add_dependency(prod_loader, "database is required to put records")
 app.synth()

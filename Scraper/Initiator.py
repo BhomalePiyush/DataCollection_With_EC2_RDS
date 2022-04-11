8c7038eddd0f9851e783c4b9e12ac055093eeb68
komalsat18 <satputekomal205@yahoo.com>
2022-04-11 20:40:30 +0530

    docstring checked in sentiment_analysis function

--- Scraper/Initiator.py
+++ Scraper/Initiator.py
@@ -69,7 +69,7 @@ def sentiment_analysis(review_list):
         return mode(labels), mean(score)
     except StatisticsError:
         return 'POSITIVE', 0.1
-
+print(sentiment_analysis.__doc__)
 
 def sentiment(product_url):
     try:
